298cb3a7a48d6bf5a10e5198ebac16eaa2303173
AnaDaGraca <aninha100.legal@hotmail.com>
2022-12-14 13:05:58 -0300

    Twelfth commit

--- js/cart.js
+++ js/cart.js
@@ -1,40 +1,44 @@
-var valorTotal = [0,0];
-var valorProduto = [6.000,00,4.000,00,3.000,00,3.000,00,4.000,00,6.000,00,2.000,00,4.000,00,3.000,00];
-var qtd = [0,0];
+var valorTotal = [0, 0];
+var valorProduto = [6000.00, 4000.00, 4000.00, 4000.00, 4000.00, 6000.00, 2000.00, 4000.00, 3000.00];
+var qtd = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-function adicionarItem(item){
+function adicionarItem(item) {
 
     var quantidade = document.getElementById("quantidade" + item);
+    // console.log(quantidade)
     var total = document.getElementById("total" + item);
+    // console.log(total)
     qtd[item] += 1;
     valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
     quantidade.innerHTML = qtd[item];
-    total.innerHTML = valorTotal[item].toFixed(4);
+    total.innerHTML = valorTotal[item].toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
     valorCompra();
 }
 
 
-function removerItem(item){
+function removerItem(item) {
 
-    if(qtd[item] > 0){
-        qtd[item] -=1;
+    if (qtd[item] > 0) {
+        qtd[item] -= 1;
         var quantidade = document.getElementById("quantidade" + item);
         var total = document.getElementById("total" + item);
         quantidade.innerHTML = qtd[item];
         valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item];
-        total.innerHTML = valorTotal[item].toFixed(4);
+        console.log(total)
+        console.log(qtd[item])
+        qtd[item] !== 0 ? total.innerHTML = valorTotal[item].toLocaleString('pt-br', { style: 'currency', currency: 'BRL' }) : true;
         valorCompra();
     }
 }
 
-function valorCompra(){
+function valorCompra() {
 
     var valorTotalCompra = document.getElementById("valorTotalCompra");
     var valor = 0;
 
-    for(var i = 0; i < valorTotal.length; i++){
+    for (var i = 0; i < valorTotal.length; i++) {
         valor += valorTotal[i];
     }
 
-    valorTotalCompra.innerHTML = valor.toFixed(4);
+    valorTotalCompra.innerHTML = valor.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 }
